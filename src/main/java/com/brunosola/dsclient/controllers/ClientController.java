ba2bd0dbcf03c2ca43fd3dac9fe5b41a0ed1d205
BrunoSola <brunosola3@gmail.com>
2024-05-20 04:35:19 -0300

    :sparkles: Feat: GET findAll clients paginado

--- src/main/java/com/brunosola/dsclient/controllers/ClientController.java
+++ src/main/java/com/brunosola/dsclient/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.brunosola.dsclient.controllers;
 import com.brunosola.dsclient.dto.ClientDTO;
 import com.brunosola.dsclient.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,5 +21,10 @@ public class ClientController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
         return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable){
+        return ResponseEntity.ok(service.findAll(pageable));
     }
 }
